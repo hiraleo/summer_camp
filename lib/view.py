90c70a363bd8927471be3787338ca10e7a1391cc
丸山拓己 <satouredstone@gmail.com>
2017-08-25 16:28:35 +0900

    merge branch

--- lib/view.py
+++ lib/view.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, url_for, render_template, flash
+from flask import request, redirect, url_for, render_template, flash, session
 from lib import app, db
 from lib.models import User
 
@@ -13,5 +13,6 @@ def login():
     password = request.form['password']
     target_user = User.query.filter(User.name == name).first()
     if target_user.password == password:
+        session['id', 'name'] = [target_user.id, name]
         return redirect('/top')
 
